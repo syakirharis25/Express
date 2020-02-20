1dc98a7dd427223362ac88ff2d882eecc209f2eb
Muhammad Syakir bin Mohd Haris <syakirharis25@gmail.com>
2020-02-20 22:05:57 +0800

    adding a lot fo files

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const path = require('path');
+const logger = require('./middleware/logger');
 
 const app = express();
 
+// Init middleware
+// app.use(logger);
+
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api/members', require('./routes/api/members'));
 
 const PORT = process.env.PORT || 5000;
 
